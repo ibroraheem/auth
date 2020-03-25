bfd4e0e5f67c522762bab7eefe7e9cf8f8a416d4
Ibrahim Abdulraheem <opeibrahim@gmail.com>
2020-03-25 07:45:56 +0100

    Added Welcome Email Templates and made a little change on verify email address page

---
+++ resources/views/emails/welcome.blade.php
@@ -0,0 +1,9 @@
+@component('mail::message')
+# Hello {{ Auth::user()->email }},
+
+We Are thrilled have you and whant to make your experience with Betafluence as smoth as possible
+
+
+
+Warmest Regards,<br> Your Friends at {{ config('app.name') }}
+@endcomponent

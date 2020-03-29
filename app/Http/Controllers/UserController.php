b5ba9c0e14b308423e867db1f79386f71982e1a6
Ibrahim Abdulraheem <opeibrahim@gmail.com>
2020-03-29 17:09:11 +0100

    Avatar Upload Functionality Added

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -37,16 +37,13 @@ class UserController extends Controller
     }
     public function update_avatar(Request $request)
     {
-        if ($request->hasFile('avatar')) {
-            $avatar = $request->file('avatar');
-            $filename = time() . '.' . $avatar->getClientOriginalExtension();
-            Image::make($avatar)->resize(300, 300)->save(public_path('/uploads/avatars/' . $filename));
-
-            $user = Auth::user();
-            $user->avatar = $filename;
-            $user->save();
-        }
-
-        return view('profile', array('user' => Auth::user()));
+        $this->validate($request, ['avatar' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',]);
+        $image = $request->file('avatar');
+        $filename = time() . '.' .$image->getClientOriginalExtension();
+        $image->move(public_path("/uploads/avatars/"), $filename);
+        $user = Auth::user();
+    		$user->avatar = $filename;
+    		$user->save();
+        return view('profile', array('user' => Auth::user()) );
     }
 }

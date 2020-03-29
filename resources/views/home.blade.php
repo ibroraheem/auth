f0e14b36a4ae13c2bf762f1d7eea32358d13e574
Ibrahim Abdulraheem <opeibrahim@gmail.com>
2020-03-29 17:44:40 +0100

    Added Cards To Dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,19 +2,51 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+
+
+    <div class="row">
+            <div class="col-sm-4">
+                <div class="card">
+                    <div class="card-body">
+                        <img class="" src="https://img.icons8.com/ios-glyphs/30/000000/user--v1.png"/> <p class="d-inline">My Profile</p>
+                        <a class="stretched-link" href="{{route('profile')}}"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="card">
+                    <div class="card-body">
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="card">
+                    <div class="card-body">
+
+                    </div>
+                </div>
+            </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-4">
             <div class="card">
-                <div class="card-header">Dashboard</div>
-
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
 
-                    You are logged in!
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4">
+            <div class="card">
+                <div class="card-body">
+
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4">
+            <div class="card">
+                <div class="card-body">
+
                 </div>
             </div>
         </div>
